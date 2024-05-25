cd94f4947eb29f58becf7752be4c94a92abd0ab0
akashsahu01 <sahuji4241@gmail.com>
2024-05-25 18:36:43 +0530

    Add files via upload

---
+++ calculater.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class calculater {
+      public static void main(String args[]){
+        Scanner cl = new Scanner(System.in);
+        int a = cl.nextDouble();
+        System.out.println("enter the first number");
+        int b = cl.nextDouble();
+        System.out.println("enter the second number");
+  
+        switch(a,b){
+        return = a+b;
+        case '+' : System.out.println(a + "+" b = "=" +return);
+        break;
+        
+            }
+        }
+}
